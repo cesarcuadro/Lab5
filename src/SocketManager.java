8d68023da64e4cf4b421603b753c9883bfea3e38
cesarcuadro <cesarcuadro777@gmail.com>
2024-09-23 15:26:23 -0700

    completed SocketManager

--- src/SocketManager.java
+++ src/SocketManager.java
@@ -12,11 +12,22 @@ public class SocketManager {
         // port of the other server
         System.out.print("Enter the port for this server to talk to: ");
         int otherPort = new Scanner(System.in).nextInt();
+
+        SocketServer oServer = new SocketServer(initialPort);
+        Thread oServerThread = new Thread(oServer);
+        oServerThread.start();
         // after starting up the server, should be a never-ending loop
-        // ask user for 3 numbers separated by commas
-        // Ex. 4,7,22
-        // send raw text to the server using the SocketClient class
-        // items sent should be comma-delimited all concatenated as one string
+        while (true) {
+            // ask user for 3 numbers separated by commas
+            // Ex. 4,7,22
+            System.out.print("Enter three numbers separated by a comma (Ex. 4,7,22): ");
+            String numberInput = new Scanner(System.in).nextLine();
+            // send raw text to the server using the SocketClient class
+            // items sent should be comma-delimited all concatenated as one string
+            SocketClient oSocketClient = new SocketClient();
+            String oServerResponse = oSocketClient.connectForOneMessage(otherIPAddress, otherPort, numberInput);
+            System.out.println(oServerResponse);
+        }
     }
 
 }
